a863533ab1c787306342633929cc3101ab331342
oscarVillango <mengo.villafuerte@gmail.com>
2017-03-18 10:43:19 -0600

    Test: Verificar Stack

---
+++ src/test/java/com/avantica/dynamicStructures/DynamicStructuresTest.java
@@ -0,0 +1,14 @@
+package com.avantica.dynamicStructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DynamicStructuresTest {
+	
+	@Test
+	public void stackTest(){
+		Stack stack = new Stack();
+		
+		Assert.assertTrue(stack);
+	}
+}

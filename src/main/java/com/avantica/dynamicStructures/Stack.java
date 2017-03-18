c805208f87148e785e9fe8a367cc2b1f2e5cc6a0
oscarVillango <mengo.villafuerte@gmail.com>
2017-03-18 13:00:32 -0600

    Solucion: agregar cuantos valores se deseen

--- src/main/java/com/avantica/dynamicStructures/Stack.java
+++ src/main/java/com/avantica/dynamicStructures/Stack.java
@@ -2,20 +2,37 @@ package com.avantica.dynamicStructures;
 
 public class Stack {
 
-	private String[] value = new String[10];
+	private Node firstElement = null;
 	private int size = 0;
+	
+	
+	private class Node{
+		private String value;
+		private Node next;
+	}
 	
 	public Boolean isEmpty() {
 		return size == 0;
 	}
 
 	public void push(final String string) {
-		value[size] = string;
+		Node oldNode = firstElement;
+		Node newNode = new Node();
+		
+		newNode.value = string;
+		firstElement = newNode;
+		firstElement.next = oldNode;
+		
 		size++;
 	}
 
 	public String pop() {
-		return value[--size];
+		Node currentNode = firstElement;
+		firstElement = firstElement.next;
+		
+		size--;
+		
+		return currentNode.value;
 	}
 
 	public int size() {

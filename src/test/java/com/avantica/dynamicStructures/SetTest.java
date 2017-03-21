dc9303996eeccccb45c7fd50cf7408f69c21dcc5
Oscar Villafuerte <oscar.villafuerte@avantica.net>
2017-03-21 14:36:14 -0600

    Test: Create Instance

--- src/test/java/com/avantica/dynamicStructures/SetTest.java
+++ src/test/java/com/avantica/dynamicStructures/SetTest.java
@@ -1,14 +1,12 @@
 package com.avantica.dynamicStructures;
 
-import java.util.Set;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SetTest {
 	@Test
 	public void verifySetInstance(){
-		Set<String> implementationSet = new ImplementationSet<String>();
+		ImplementationSet implementationSet = new ImplementationSet();
 		
 		Assert.assertFalse(implementationSet == null);
 	}
